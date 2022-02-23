3d7e3b42faadb87646d4cc1c880f7008e04b8b47
nalek0 <belash.aleksandr777@gmail.com>
2022-02-23 17:51:30 +0300

    Small fixes and better documentation

--- README.md
+++ README.md
@@ -1,23 +1,39 @@
 # Set status
 
 ## config.json format:
+
 ```json
 {
 	"user_info": {
-		"token": "your_token"
+		"token": "..."
 	},
 
 	"stream_info": {
-		"details": "Some string",
+		"name": "...",
+		"details": "...",
+		"state": "...",
 
+		"application_id": 1234567890,
 		"assets": {
-			"application_id": 000000000000000,
-
-			"large_image": "You'r asset's image id",
-			"large_text": "Some line"
+			"large_image": "1234567890",
+			"large_text": "...",
+			"small_image": "1234567890",
+			"small_text": "..."
 		},
 
-		"url": "Where you want to redirect user, if he press button"
+		"url": "..."
 	}
 }
 ```
+
+## What means what?
+
+* **token** - your token for [https://discord.com/](https://discord.com/). I recommend you to search in google, how to get it.
+* **name** - the name of the activity. I don't know why, but this is mandatory parameter
+* **details** - the title of the activity
+* **state** - the game of the activity
+* **application_id** - id of the application, where discord will search images for the activity icons
+* **large_image**, **small_image** - ids of the images
+(see `Rich Presence/Art Assets` in discord developers portal or in the url `https://discord.com/developers/applications/<application_id>/rich-presence/assets`)
+* **large_text**, **small_text** - Text, that will be shown when you hover activity images
+* **url** - "url of the stream", but you can put every url you want, the button under the activity will redirect you to this url
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import datetime
-import requests
-import discord
-import base64
 import json
 
+import discord
+
+config = {}
 try:
 	f = open("config.json", 'r')
 	config = json.load(f)
@@ -18,27 +18,23 @@ class MyClient(discord.Client):
 	async def on_ready(self):
 		print(self.user, "is ready!")
 
-		# Seting streaming status:
+		# Setting streaming status:
 		activity = discord.Activity(
-			application_id 	= config['stream_info']['assets']['application_id'],
-			assets 			= { 
-				"large_image": config['stream_info']['assets']['large_image'], 
-				"large_text": config['stream_info']['assets']['large_text'] 
-			},
-			
-			details			= config['stream_info']['details'],
-			name			= "Stream",
-			
-			start 			= datetime.datetime.now(),
-			type 			= discord.ActivityType.streaming,
-			url				= config['stream_info']['url'])
-
+			name=config['stream_info']['name'],
+			type=discord.ActivityType.streaming,
+			state=config['stream_info']['state'],
+			details=config['stream_info']['details'],
+			start=datetime.datetime.now(),
+			application_id=config['stream_info']['application_id'],
+			assets=config['stream_info']['assets'],
+			url=config['stream_info']['url'])
 
 		await self.change_presence(
-			status 		= discord.Status.idle,
-			activity	= activity)
-
+			status=discord.Status.idle,
+			activity=activity)
 		print("Status changed!")
 
-client = MyClient()
-client.run(config['user_info']['token'], bot = False)
+
+if __name__ == "__main__":
+	client = MyClient()
+	client.run(config['user_info']['token'], bot=False)
